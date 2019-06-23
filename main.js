59bddbbeae17c6892bde0594c7cc21305c374721
Varannchik <varannchik@gmail.com>
2019-06-23 18:38:52 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,31 +2,38 @@
 let calc={
     value1:'null',
     value2:'null',
-    sum:()=>{ 
-        calc.read();
-        document.querySelector('#val1').value=this.value1+this.value2;
-        document.querySelector('#val2').value=0;            
+    sum:function(){
+        this.read();
+        document.querySelector('#val1').value=value1 + value2;
+        document.querySelector('#val2').value=0;
+                
     },
-    minus:()=>{
-        calc.read(); 
-        document.querySelector('#val1').value=this.value1-this.value2;
+    minus:function(){
+        this.read();       
+        document.querySelector('#val1').value=value1 - value2;
         document.querySelector('#val2').value=0;
     },
-    div:()=>{
-        calc.read();
-        document.querySelector('#val1').value=this.value1/this.value2;
+    div:function(){ 
+        this.read();      
+        document.querySelector('#val1').value=value1 / value2;
         document.querySelector('#val2').value=0;
     },
-    mult:()=>{
-        calc.read(); 
-        document.querySelector('#val1').value=this.value1*this.value2;
+    mult:function(){
+        this.read();         
+        document.querySelector('#val1').value=value1 * value2;
         document.querySelector('#val2').value=0;
     },
     read:function(){
         value1=+document.querySelector('#val1').value;
         value2=+document.querySelector('#val2').value;
-    }
+    } 
 };
+
+
+
+
+
+
 
 
 
