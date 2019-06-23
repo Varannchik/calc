00c16dbf9ba5be8c47f130978c3650b713f4a6a6
Varannchik <varannchik@gmail.com>
2019-06-23 16:24:24 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+let elem=document.querySelector('.container');
+let h2=document.createElement('h2');
+let btn=document.querySelector('button');
+//let h3=document.querySelector('h3');
+let div2=document.querySelector('.dqs');
+
+// h2.innerHTML='Hello,Word!';
+// h2.className='title';
+// elem.appendChild(h2);
+
+// btn.addEventListener('click',function(){
+//     let p=document.createElement('p');
+//     let input=document.querySelector('input').value; 
+//     let bMinus=document.createElement('button');
+//     //elem.insertBefore(p, elem.children[0]);
+//     elem.appendChild(p);
+//     p.innerHTML=input;
+//     p.appendChild(bMinus);
+//     bMinus.innerHTML='-';
+
+//     bMinus.addEventListener('click',function(){    
+//         elem.removeChild(p);
+//     });
+       
+// });
+
+
+btn.addEventListener('click',function(){
+    let p=document.createElement('p');
+    let input=document.querySelector('input').value; 
+    let bMinus=document.createElement('button');
+    let span=document.createElement('span');
+    let bArrowUp=document.createElement('button');
+    let bArrowDown=document.createElement('button');
+    div2.appendChild(p);
+    p.innerHTML=input;
+    p.appendChild(bMinus);
+    p.appendChild(bArrowUp);
+    p.appendChild(span);
+    p.appendChild(bArrowDown);
+    bMinus.innerHTML='-';
+    bArrowUp.innerHTML='arrow UP';
+    bArrowDown.innerHTML='arrow Down';
+    
+    let arr2=document.querySelectorAll('span'); 
+    for(let i=0; i<arr2.length; i++){
+        arr2[i].innerHTML=''+(i+1);        
+        
+    } 
+    
+    bMinus.addEventListener('click',function(){    
+        div2.removeChild(p);
+        let arr=document.querySelectorAll('span');        
+        for(let j=0; j<=arr.length; j++){
+            arr[j].innerHTML=''+(j+1);            
+        }
+    });
+    
+    bArrowUp.addEventListener('click',function(){        
+        div2.insertBefore(p, p.previousElementSibling);
+    });
+    bArrowDown.addEventListener('click',function(){        
+        div2.insertBefore(p, p.nextSibling.nextSibling);
+    });        
+});
+
+//div2.removeChild(h3);
+
+
+
