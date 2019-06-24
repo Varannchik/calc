75426410f9a7f4ce6f77f231962416d9fa7f1b9b
Varannchik <varannchik@gmail.com>
2019-06-24 09:14:09 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,31 +3,32 @@ let calc={
     value1:'null',
     value2:'null',
     sum:function(){
-        this.read();
+        
         document.querySelector('#val1').value=this.value1 + this.value2;
         document.querySelector('#val2').value=0;
                 
     },
     minus:function(){
-        this.read();       
-        document.querySelector('#val1').value=value1 - value2;
+               
+        document.querySelector('#val1').value=this.value1 - this.value2;
         document.querySelector('#val2').value=0;
     },
     div:function(){ 
-        this.read();      
-        document.querySelector('#val1').value=value1 / value2;
+              
+        document.querySelector('#val1').value=this.value1 / this.value2;
         document.querySelector('#val2').value=0;
     },
     mult:function(){
-        this.read();         
-        document.querySelector('#val1').value=value1 * value2;
+               
+        document.querySelector('#val1').value=this.value1 * this.value2;
         document.querySelector('#val2').value=0;
     },
     read:function(){
-        let value1=+document.querySelector('#val1').value;
-        let value2=+document.querySelector('#val2').value;
+        this.value1=+document.querySelector('#val1').value;
+        this.value2=+document.querySelector('#val2').value;
     } 
 };
+calc.read();
 
 
 
